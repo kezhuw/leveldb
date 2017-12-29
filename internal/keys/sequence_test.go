054a9a6159d2d77ece3d249c88bd198891f6ff11
Kezhu Wang <kezhuw@gmail.com>
2017-12-29 11:00:12 +0800

    Rename test case type for Sequence.Next

--- internal/keys/sequence_test.go
+++ internal/keys/sequence_test.go
@@ -6,13 +6,13 @@ import (
 	"github.com/kezhuw/leveldb/internal/keys"
 )
 
-type sequenceTest struct {
+type nextSequenceTest struct {
 	seq  keys.Sequence
 	next uint64
 	want keys.Sequence
 }
 
-var sequenceTests = []sequenceTest{
+var nextSequenceTests = []nextSequenceTest{
 	{
 		seq:  0x00123456789abcde,
 		next: 0x000fc9a8743210fe,
@@ -26,7 +26,7 @@ var sequenceTests = []sequenceTest{
 }
 
 func TestSequenceNext(t *testing.T) {
-	for i, test := range sequenceTests {
+	for i, test := range nextSequenceTests {
 		got := test.seq.Next(test.next)
 		if got != test.want {
 			t.Errorf("test=%d sequence=%#x next=%d got=%#x want=%#x", i, test.seq, test.next, got, test.want)
