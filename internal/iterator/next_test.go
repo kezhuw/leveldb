6f286b24779dc7f61a07ee35e712460d9a774520
Kezhu Wang <kezhuw@gmail.com>
2018-01-09 10:05:57 +0800

    Add error test for iterator.Next

--- internal/iterator/next_test.go
+++ internal/iterator/next_test.go
@@ -20,24 +20,43 @@ var nextIteratorTests = []nextIteratorTest{
 			{key: "h"},
 		},
 	},
+	{
+		entries: []iterationEntry{
+			{key: "b"},
+			{key: "d"},
+			{err: errFoo, key: "e"},
+			{key: "f"},
+			{key: "h"},
+		},
+	},
 }
 
-func appendNext(dst []iterationEntry, it iterator.Iterator) []iterationEntry {
+func appendNext(dst []iterationEntry, it iterator.Iterator) ([]iterationEntry, error) {
 	for iterator.Next(it) {
 		dst = append(dst, iterationEntry{
 			key:   string(it.Key()),
 			value: string(it.Value()),
 		})
 	}
-	return dst
+	return dst, it.Err()
+}
+
+func appendEntries(dst []iterationEntry, entries []iterationEntry) ([]iterationEntry, error) {
+	for i, e := range entries {
+		if e.err != nil {
+			return append(dst, entries[:i]...), e.err
+		}
+	}
+	return append(dst, entries...), nil
 }
 
 func TestNext(t *testing.T) {
 	for i, test := range nextIteratorTests {
 		it := newSliceIterator(test.entries)
-		got := appendNext(nil, it)
-		if !matchEntries(got, test.entries) {
-			t.Errorf("test=%d got=%v want=%v", i, got, test.entries)
+		gotEntries, gotErr := appendNext(nil, it)
+		wantEntries, wantErr := appendEntries(nil, test.entries)
+		if !(matchEntries(gotEntries, wantEntries) && gotErr == wantErr) {
+			t.Errorf("test=%d got=(%v,%s) want=(%v,%s)", i, gotEntries, gotErr, wantEntries, wantErr)
 		}
 	}
 }
