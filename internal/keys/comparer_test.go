9265a09ce650397f84773912cbd9fe0453a89f0b
Kezhu Wang <kezhuw@gmail.com>
2018-01-09 11:14:13 +0800

    Promote test coverage for keys/comparer.go

--- internal/keys/comparer_test.go
+++ internal/keys/comparer_test.go
@@ -16,39 +16,63 @@ func (bytewiseComparer) Compare(a, b []byte) int {
 var bytesComparer keys.Comparer = bytewiseComparer{}
 
 type comparerTest struct {
+	a   []byte
+	b   []byte
 	min []byte
 	max []byte
 }
 
 var comparerTests = []comparerTest{
 	{
-		min: []byte("aaaa"),
-		max: []byte("aaaa"),
+		a: []byte("aaaa"),
+		b: []byte("aaaa"),
 	},
 	{
-		min: []byte("aaaa"),
-		max: []byte("aaab"),
+		a: []byte("aaaa"),
+		b: []byte("aaab"),
 	},
 	{
-		min: []byte("aaaa"),
-		max: []byte("bbbb"),
+		a: []byte("aaaa"),
+		b: []byte("bbbb"),
 	},
+	{
+		a: []byte("bbbb"),
+		b: []byte("aaaa"),
+	},
+}
+
+func init() {
+	for i, n := 0, len(comparerTests); i < n; i++ {
+		t := &comparerTests[i]
+		a, b := t.a, t.b
+		switch bytes.Compare(a, b) {
+		case 0:
+			t.min = a
+			t.max = a
+		case -1:
+			t.min = a
+			t.max = b
+		case +1:
+			t.min = b
+			t.max = a
+		}
+	}
 }
 
 func TestComparerMax(t *testing.T) {
 	for i, test := range comparerTests {
-		got := keys.Max(bytesComparer, test.min, test.max)
+		got := keys.Max(bytesComparer, test.a, test.b)
 		if !bytes.Equal(got, test.max) {
-			t.Errorf("test=%d min=%x max=%x got=%x", i, test.min, test.max, got)
+			t.Errorf("test=%d a=%q b=%q got=%q want=%q", i, test.a, test.b, got, test.max)
 		}
 	}
 }
 
 func TestComparerMin(t *testing.T) {
 	for i, test := range comparerTests {
-		got := keys.Min(bytesComparer, test.min, test.max)
+		got := keys.Min(bytesComparer, test.a, test.b)
 		if !bytes.Equal(got, test.min) {
-			t.Errorf("test=%d min=%x max=%x got=%x", i, test.min, test.max, got)
+			t.Errorf("test=%d a=%q b=%q got=%q want=%q", i, test.a, test.b, got, test.min)
 		}
 	}
 }
